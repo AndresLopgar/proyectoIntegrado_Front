f262dd6122bebc697a80086c99cb34fbc8e868e3
AndresLopgar <andreslopgar17@gmail.com>
2024-04-03 16:57:20 +0200

    desarrollo parcial componente buscar

--- src/app/components/buscar/buscar.component.ts
+++ src/app/components/buscar/buscar.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-buscar',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, CommonModule],
   templateUrl: './buscar.component.html',
   styleUrl: './buscar.component.scss'
 })
 export class BuscarComponent {
 
+  holaLog(){
+    console.log("hola");
+    
+  }
 }
