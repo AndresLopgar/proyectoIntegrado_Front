35cb3814bf0c8a283f423d9c6e7a7ce68800c06c
andres jesús lópez garcía <andreslopgar17@gmail.com>
2024-04-08 09:54:43 +0200

    pie y cabecera responsive

--- src/app/layout/cabecera/cabecera.component.ts
+++ src/app/layout/cabecera/cabecera.component.ts
@@ -12,7 +12,7 @@ import { filter } from 'rxjs';
   styleUrl: './cabecera.component.scss'
 })
 export class CabeceraComponent implements OnInit{
-  tipoUsuario: string = "registrado";
+  tipoUsuario: string = "gestor";
   titulo: string = "";
 
   constructor(
