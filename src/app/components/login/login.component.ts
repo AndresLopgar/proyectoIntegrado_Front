0fcd60ccce2c3716eaa44eb87911cb541b72badb
AndresLopgar <andreslopgar17@gmail.com>
2024-04-09 16:53:34 +0200

    formateo login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,16 +3,21 @@ import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ButtonModule, FormsModule],
+  imports: [ButtonModule, FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   @ViewChild('loginForm') loginForm!: NgForm; // Referencia al formulario
+
+  showPassword: boolean = false;
+  iconName: string = 'pi pi-lock';
+
 
   constructor(private router: Router) {}
   loginData = {
@@ -25,6 +30,19 @@ export class LoginComponent {
     console.log('Datos de login:', this.loginData);
     this.router.navigateByUrl('/home'); // Redirige a la ruta '/home'
   }
+
+  togglePasswordVisibility() {
+    if (!this.showPassword) {
+      this.showPassword = true;
+      const inputField = document.getElementById('contraseñaLogin') as HTMLInputElement;
+      inputField.type = 'text';
+    } else {
+      this.showPassword = false;
+      const inputField = document.getElementById('contraseñaLogin') as HTMLInputElement;
+      inputField.type = 'password';
+    }
+  }
+  
 
   borrar() {
     this.loginForm.reset(); // Resetea el formulario utilizando el método reset() de PrimeNG
