b7f0fe0cedd8afe1edc8b6691b2b767b1400a95f
andres jesús lópez garcía <andreslopgar17@gmail.com>
2024-05-10 15:50:05 +0200

    update amistades.service

--- src/app/services/amistad.service.ts
+++ src/app/services/amistad.service.ts
@@ -7,7 +7,10 @@ import { Amistad } from '../model/amistad';
   providedIn: 'root',
 })
 export class AmistadService {
+  //heroku
+  //private baseUrl = 'https://artconnect-9bc127224463.herokuapp.com/api/amistades';
 
+  //localhost
   private baseUrl = 'http://localhost:8081/api/amistades';
 
   constructor(private http: HttpClient) { }
