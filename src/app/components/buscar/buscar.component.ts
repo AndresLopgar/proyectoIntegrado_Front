5f0b7645c223b6ceb0a991ef8d55f3c5eb3e2f42
andres jesús lópez garcía <andreslopgar17@gmail.com>
2024-05-10 11:02:16 +0200

    filtrado por nombre

--- src/app/components/buscar/buscar.component.ts
+++ src/app/components/buscar/buscar.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
-import { Usuario} from '../../model/usuario';
+import { Usuario } from '../../model/usuario';
 import { UsuarioService } from '../../services/usuario.service';
 import { HttpClientModule } from '@angular/common/http';
 import { Router } from '@angular/router';
@@ -13,32 +13,41 @@ import { Compania } from '../../model/compania';
   standalone: true,
   imports: [ButtonModule, CommonModule, HttpClientModule],
   templateUrl: './buscar.component.html',
-  styleUrl: './buscar.component.scss'
+  styleUrls: ['./buscar.component.scss']
 })
-export class BuscarComponent implements OnInit{
+export class BuscarComponent implements OnInit {
   companias: Compania[] = [];
   usuarios: Usuario[] = [];
+  usuariosFiltrados: Usuario[] = [];
+  companiasFiltradas: Compania[] = [];
   usuarioLocalStorage: Usuario | null = null;
 
-  constructor(private usuarioService: UsuarioService, private router: Router, private companiaService: CompaniaService) { }
+  constructor(
+    private usuarioService: UsuarioService,
+    private router: Router,
+    private companiaService: CompaniaService
+  ) {}
 
   ngOnInit(): void {
     this.getAllUsuarios();
     this.getAllCompanias();
     const usuarioString = localStorage.getItem('usuario');
-  if (usuarioString !== null) {
-    this.usuarioLocalStorage = JSON.parse(usuarioString);
-  }
+    if (usuarioString !== null) {
+      this.usuarioLocalStorage = JSON.parse(usuarioString);
+    }
   }
 
   getAllUsuarios() {
     this.usuarioService.getAllUsuarios().subscribe(
       usuarios => {
         if (this.usuarioLocalStorage) {
-          this.usuarios = usuarios.filter(usuario => usuario.id !== this.usuarioLocalStorage?.id);
+          this.usuarios = usuarios.filter(
+            usuario => usuario.id !== this.usuarioLocalStorage?.id
+          );
         } else {
           this.usuarios = usuarios;
         }
+        this.usuariosFiltrados = [...this.usuarios]; // Inicialmente, mostrar todos los usuarios
       },
       error => {
         console.log('Error al recuperar usuarios:', error);
@@ -50,35 +59,41 @@ export class BuscarComponent implements OnInit{
     this.companiaService.getAllCompanias().subscribe(
       companias => {
         if (this.usuarioLocalStorage && this.usuarioLocalStorage.companiaSeguida) {
-          this.companias = companias.filter(compania => compania.id !== this.usuarioLocalStorage!.companiaSeguida);
+          this.companias = companias.filter(
+            compania => compania.id !== this.usuarioLocalStorage!.companiaSeguida
+          );
         } else {
           this.companias = companias;
         }
+        this.companiasFiltradas = [...this.companias]; // Inicialmente, mostrar todas las compañías
       },
       error => {
         console.log('Error al recuperar compañías:', error);
       }
     );
   }
+
+  filtrarUsuariosYCompanias(event: any) {
+    const texto = (event.target as HTMLInputElement).value.trim(); // Eliminamos espacios en blanco al inicio y al final
+    this.usuariosFiltrados = this.usuarios.filter(
+      usuario =>
+        usuario.nombre.toUpperCase().startsWith(texto.toUpperCase())
+    );
   
-
-  usuarioEstandarChecked: boolean = false;
-
-  toggleCamposUsuarioEstandar(event: any) {
-    this.usuarioEstandarChecked = event.target.checked;
+    this.companiasFiltradas = this.companias.filter(compania =>
+      compania.nombre.toUpperCase().startsWith(texto.toUpperCase())
+    );
   }
 
   goPerfil(usuarioId: number) {
-    this.router.navigate(['/perfil', usuarioId]); // Suponiendo que la ruta para el perfil sea '/perfil'
+    this.router.navigate(['/perfil', usuarioId]);
   }
 
-  goCompania(companiaID: number){
+  goCompania(companiaID: number) {
     this.router.navigate(['/compania', companiaID]);
   }
 
-seguirUsuario(event: Event) {
-  event.stopPropagation(); // Evitar la propagación del evento clic
-}
-
-
+  seguirUsuario(event: Event) {
+    event.stopPropagation(); // Evitar la propagación del evento clic
+  }
 }
